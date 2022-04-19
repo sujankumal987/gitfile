a76e3502d3a6b87664b44941b87729021b7b6735
sujan kumal <sujanmaskey987@gmail.com>
2022-04-19 23:10:30 +0545

    second commit

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@ public class Diff {
     public static void main(String[] args) {
         int a = 3;
         int b= 6;
-        System.out.println ("The sum is" + (a-b));
+        System.out.println ("The Diff is" + (a-b));
     }
     
 }
